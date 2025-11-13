8187bbc8d37e4ca3cf8f01f524ad1b7ca165532a
Cesar Enrique Pozo Vasquez <cesarpozo25@gmail.com>
2025-11-13 01:46:09 +0100

    tabla tienda hecha

--- CORE1/Pages/Tiendas.cshtml.cs
+++ CORE1/Pages/Tiendas.cshtml.cs
@@ -1,12 +1,18 @@
+using CORE1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CORE1.Pages
 {
-    public class Index1Model : PageModel
+    public class TiendasModel : PageModel
     {
+        [BindProperty]
+        public List<Tienda> Tiendas { get; set; }
         public void OnGet()
         {
+            Services.Service service = new Services.Service();
+            Tiendas = service.GetTiendas();
         }
+
     }
 }
